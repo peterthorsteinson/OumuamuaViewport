18bc7de671a27780f288559067568169d5991a0f
Peter <peterteach@gmail.com>
2020-07-23 07:42:57 -0700

    initial commit

---
+++ Assets/Scripts/TumbleOumuamuaScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TumbleOumuamuaScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Tumble Oumuamua
+        transform.Rotate(Vector3.up * Time.deltaTime * 80.0f);
+        transform.Rotate(Vector3.right * Time.deltaTime * 20.0f);
+    }
+}
